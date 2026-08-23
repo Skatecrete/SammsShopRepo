7bcc0b9fe56ed1e18edd2b37e91c10ef6eeb2b7c
Skatecrete <skatecrete@gmail.com>
2026-08-23 12:42:34 -0400

    Add files via upload

---
+++ netlify/functions/get-token.js
@@ -0,0 +1,13 @@
+exports.handler = async () => {
+    const token = process.env.GITHUB_TOKEN;
+    if (!token) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'GitHub token not configured' })
+        };
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ token })
+    };
+};
